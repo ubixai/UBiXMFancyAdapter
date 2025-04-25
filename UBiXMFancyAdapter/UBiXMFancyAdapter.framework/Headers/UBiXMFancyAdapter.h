686eb809211abd287e77fc48bae04b8024a377dd
guoqiang <guoqiang.zhu@ubixai.com>
2025-04-25 10:58:15 +0800

    update 2.2.61.0

---
+++ UBiXMFancyAdapter/UBiXMFancyAdapter.framework/Headers/UBiXMFancyAdapter.h
@@ -0,0 +1,23 @@
+//
+//  UBiXMFancyAdapter.h
+//  UBiXMFancyAdapter
+//
+//  Created by guoqiang.zhu on 2025/3/24.
+//  Copyright © 2025 UBiX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//@interface UBiXMFancyAdapter : NSObject
+//
+//@end
+
+//! Project version number for UBiXMFancyAdapter.
+FOUNDATION_EXPORT double UBiXMFancyAdapterVersionNumber;
+
+//! Project version string for UBiXMFancyAdapter.
+FOUNDATION_EXPORT const unsigned char UBiXMFancyAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+
+
